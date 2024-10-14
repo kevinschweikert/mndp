0c59d624f89f2844aaf63cb287bb0444482dd8c9
Frank Hunleth <fhunleth@troodon-software.com>
2024-10-14 10:10:53 +0200

    Fix discover.exs warnings and error
    This gets the discover.exs working again. It had a dependency conflict
    with owl and logged a warning due to the `start_applications` parameter
    being ignored.

--- discover.exs
+++ discover.exs
@@ -4,9 +4,13 @@
 Mix.start()
 Mix.target(:host)
 
-Mix.install([
-  {:mndp, "~> 0.1.0", start_applications: false},
-  {:owl, "~> 0.11.0"}
-])
+Mix.install(
+  [
+    {:mndp, "~> 0.1.0"},
+    {:owl, "~> 0.12.0"}
+  ],
+  start_applications: false
+)
 
+Application.ensure_all_started(:owl)
 MNDP.CLI.run()
