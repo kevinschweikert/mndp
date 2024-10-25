3ed4d830341e5edbc116a02ab39d50b1901e825c
Kevin Schweikert <kevin@kevinschweikert.de>
2024-10-25 20:46:15 +0200

    remove owl dependency

--- lib/mndp/cli.ex
+++ lib/mndp/cli.ex
@@ -7,14 +7,6 @@ defmodule MNDP.CLI do
 
   @impl GenServer
   def init(:ok) do
-    Owl.LiveScreen.add_block(:discovered,
-      state: :init,
-      render: fn
-        :init -> "Scanning for devices..."
-        :update -> MNDP.list_discovered() |> MNDP.Render.to_owl_table()
-      end
-    )
-
     {:ok, [], {:continue, :subscribe}}
   end
 
@@ -26,14 +18,12 @@ defmodule MNDP.CLI do
 
   @impl GenServer
   def handle_info({:mndp, _mndp}, socket) do
-    Owl.LiveScreen.update(:discovered, :update)
     {:noreply, socket}
   end
 
   def run() do
     Logger.configure(level: :info)
     Application.ensure_all_started(:mndp)
-    Application.ensure_all_started(:owl)
 
     IO.puts("""
 
